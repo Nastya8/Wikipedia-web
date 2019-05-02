66fc55231aa5de52f8e7b2d887fbcf2087bb17e9
Nastya8 <korsarushka@yandex.ru>
2019-05-02 15:38:46 +0300

    created class AddToFavorites, created TestBase

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    WebDriver wd;
+
+    @BeforeClass
+    public void setUp(){
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wd.navigate().to("https://www.wikipedia.org//");
+    }
+
+    public void click(By locator) {
+        wd.findElement(locator).click();
+    }
+
+    public void type(By locator, String text) {
+        click(locator);
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+    }
+
+    @AfterClass
+            public void tearDown(){
+        wd.quit();
+    }
+}
